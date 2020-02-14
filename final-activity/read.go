f267e1c8fe7f4fdb8a830d8075a1ffd70d7ecb92
Jacinto Mba Cantero <jacmba@gmailcom>
2020-02-14 02:11:27 +0000

    Implemented final course project

---
+++ final-activity/read.go
@@ -0,0 +1,63 @@
+/* Final Course activity: Read file
+ *
+ * Author: Jacinto Mba <jacmba(at)gmail(dot)com>
+ *
+ * https://github.com/jacmba/coursera-go-introduction
+ *
+ * Write a program which reads information from a file and represents it in a slice of structs.
+ * Assume that there is a text file which contains a series of names. Each line of the text file
+ * has a first name and a last name, in that order, separated by a single space on the line.
+ *
+ * Your program will define a name struct which has two fields, fname for the first name,
+ * and lname for the last name. Each field will be a string of size 20 (characters).
+ *
+ * Your program should prompt the user for the name of the text file. Your program will successively
+ * read each line of the text file and create a struct which contains the first and last names
+ * found in the file. Each struct created will be added to a slice, and after all lines have been read
+ * from the file, your program will have a slice containing one struct for each line in the file.
+ * After reading all lines from the file, your program should iterate through your slice of structs
+ * and print the first and last names found in each struct.
+ */
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type person struct {
+	fname, lname string
+}
+
+func main() {
+	var fileName string
+	people := make([]person, 0)
+
+	fmt.Print("Gimme the file name: ")
+	fmt.Scan(&fileName)
+
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		dat := strings.Split(scanner.Text(), " ")
+		p := person{
+			dat[0],
+			dat[1],
+		}
+		people = append(people, p)
+	}
+
+	for _, p := range people {
+		fmt.Println(p.fname, p.lname)
+	}
+}
